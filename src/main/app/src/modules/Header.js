6a956d0f08261abf8c5c072791f7a1698b2db6af
Pelle Karlsson <per.karlsson.5@consultant.volvo.com>
2017-09-14 16:53:06 +0200

    Added a home page

---
+++ src/main/app/src/modules/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import logo from './../freesbee.jpg';
+
+const Header = () => (
+  <div className="App-header">
+    <h1>Welcome to the game 1025!</h1>
+  	<img src={logo} className="App-logo" alt="logo" />
+  </div>
+)
+
+export default Header
